7b0d4ac43cf43c0758e0b47c5867759758e9f6c3
melisa-02 <micuenta.com.bo@gmail.com>
2024-09-03 08:25:18 -0500

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,511 @@
+const productos = [
+    // polleras
+    {
+        id: "pollera-01",
+        titulo: "pollera 01",
+        imagen: "pollera1.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-02",
+        titulo: "pollera 02",
+        imagen: "pollera2.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-03",
+        titulo: "pollera 03",
+        imagen: "pollera3.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-04",
+        titulo: "pollera 04",
+        imagen: "pollera4.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-05",
+        titulo: "pollera 05",
+        imagen: "pollera5.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-06",
+        titulo: "pollera 06",
+        imagen: "pollera6.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-07",
+        titulo: "pollera 07",
+        imagen: "pollera7.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-08",
+        titulo: "pollera 08",
+        imagen: "pollera8.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-09",
+        titulo: "pollera 09",
+        imagen: "pollera9.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-010",
+        titulo: "pollera 010",
+        imagen: "pollera10.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollera-011",
+        titulo: "pollera 012",
+        imagen: "pollera12.jpg",
+        categoria: {
+            nombre: "polleras",
+            id: "polleras"
+        },
+        precio: 1000
+    },
+
+    
+    // collares
+    {
+        id: "collar-01",
+        titulo: "Collar 01",
+        imagen: "collar1.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-02",
+        titulo: "Collar 02",
+        imagen: "collar2.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-03",
+        titulo: "Collar 03",
+        imagen: "collar3.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-04",
+        titulo: "Collar 04",
+        imagen: "collar4.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-05",
+        titulo: "Collar 05",
+        imagen: "collar5.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-06",
+        titulo: "Collar 06",
+        imagen: "collar6.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-07",
+        titulo: "Collar 07",
+        imagen: "collar7.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    {
+        id: "collar-08",
+        titulo: "Collar 08",
+        imagen: "collar8.jpg",
+        categoria: {
+            nombre: "collares",
+            id: "collares"
+        },
+        precio: 100
+    },
+    // bolsas
+    {
+        id: "bolsa-01",
+        titulo: "bolsa 01",
+        imagen: "bolsa1.jpg",
+        categoria: {
+            nombre: "bolsass",
+            id: "bolsas"
+        },
+        precio: 200
+    },
+    {
+        id: "bolsa-02",
+        titulo: "bolsa 02",
+        imagen: "bolsa2.jpg",
+        categoria: {
+            nombre: "bolsass",
+            id: "bolsas"
+        },
+        precio: 200
+    },
+    {
+        id: "bolsa-03",
+        titulo: "bolsa 03",
+        imagen: "bolsa3.jpg",
+        categoria: {
+            nombre: "bolsass",
+            id: "bolsas"
+        },
+        precio: 200
+    },
+    {
+        id: "bolsa-04",
+        titulo: "bolsa 04",
+        imagen: "bolsa4.jpg",
+        categoria: {
+            nombre: "bolsass",
+            id: "bolsas"
+        },
+        precio: 200
+    },
+    {
+        id: "bolsa-05",
+        titulo: "bolsa 05",
+        imagen: "bolsa5.jpg",
+        categoria: {
+            nombre: "bolsass",
+            id: "bolsas"
+        },
+        precio: 200
+    },
+
+// zapatillas
+{
+    id: "zapato-01",
+    titulo: "zapato 01",
+    imagen: "zapato1.jpg",
+    categoria: {
+        nombre: "zapatillas",
+        id: "zapatillas"
+    },
+    precio: 200
+},
+{
+    id: "zapato-02",
+    titulo: "zapato 02",
+    imagen: "zapato2.jpg",
+    categoria: {
+        nombre: "zapatillas",
+        id: "zapatillas"
+    },
+    precio: 200
+},
+{
+    id: "zapato-03",
+    titulo: "zapato 03",
+    imagen: "zapato3.jpg",
+    categoria: {
+        nombre: "zapatillas",
+        id: "zapatillas"
+    },
+    precio: 200
+},
+{
+    id: "zapato-04",
+    titulo: "zapato 04",
+    imagen: "zapato4.jpg",
+    categoria: {
+        nombre: "zapatillas",
+        id: "zapatillas"
+    },
+    precio: 200
+},
+{
+    id: "zapato-05",
+    titulo: "zapato 05",
+    imagen: "zapato5.jpg",
+    categoria: {
+        nombre: "zapatillas",
+        id: "zapatillas"
+    },
+    precio: 200
+},
+//blusas
+{
+    id: "blusa1-01",
+    titulo: "blusa01",
+    imagen: "blusa1.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-02",
+    titulo: "blusa02",
+    imagen: "blusa2.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-03",
+    titulo: "blusa03",
+    imagen: "blusa3.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-04",
+    titulo: "blusa04",
+    imagen: "blusa4.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-05",
+    titulo: "blusa05",
+    imagen: "blusa5.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-06",
+    titulo: "blusa06",
+    imagen: "blusa6.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-07",
+    titulo: "blusa07",
+    imagen: "blusa7.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-08",
+    titulo: "blusa08",
+    imagen: "blusa8.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-09",
+    titulo: "blusa09",
+    imagen: "blusa9.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-010",
+    titulo: "blusa010",
+    imagen: "blusa10.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-011",
+    titulo: "blusa011",
+    imagen: "blusa11.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-012",
+    titulo: "blusa012",
+    imagen: "blusa12.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+{
+    id: "blusa1-013",
+    titulo: "blusa013",
+    imagen: "blusa13.jpg",
+    categoria: {
+        nombre: "blusas",
+        id: "blusas"
+    },
+    precio: 200
+},
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+
+function cargarProductos(productosElegidos) {
+    contenedorProductos.innerHTML = "";
+productosElegidos.forEach(producto => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+        <img class="producto-imagen" src=" ${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo}</h3>
+            <p class="producto-precio">$${producto.precio}</p>
+            <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>
+    `;
+    contenedorProductos.append(div);
+})
+actualizarBotonesAgregar();
+} 
+cargarProductos(productos);
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }    
+
+   })
+})
+
+
+//carrito
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+} // a partir de aqui copear
+let productosEnCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+  
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+    actualizarNumerito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
